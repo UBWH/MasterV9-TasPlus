c5f1e47355534ac271d61f620c51b2fc9b19be2b
Rob <rob.clark56+github@gmail.com>
2021-01-14 13:11:38 +0800

    Version Updated

--- tasmota/tasmota_version.h
+++ tasmota/tasmota_version.h
@@ -20,6 +20,7 @@
 #ifndef _TASMOTA_VERSION_H_
 #define _TASMOTA_VERSION_H_
 
-const uint32_t VERSION = 0x09020000;
-
+const uint32_t VERSION = 0x09020000
+                             + 0x01        //Migrate TasPlus V8->V9
+                        ;
 #endif  // _TASMOTA_VERSION_H_
